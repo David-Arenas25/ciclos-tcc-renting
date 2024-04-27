de6a225da75241df0a84663b714368b22fbad56c
jaimezzapata <zapataval2304@gmail.com>
2024-04-27 11:47:31 -0500

    Funciones declarativas y de expresion

---
+++ funciones.js
@@ -0,0 +1,31 @@
+// iniciarSesion()
+
+// function iniciarSesion() {
+//   console.log("Inicio de sesión Holis...");
+// }
+// iniciarSesion()
+
+// function iniciarSesion(){
+//     console.log("Inicio de sesión correcto...");
+// }
+// iniciarSesion()
+
+
+// var iniciarSesion = function () {
+//     console.log("Inicio de sesión correcto...");
+// };
+
+let iniciarSesion = () => {
+    console.log("Inicio de sesión correcto...");
+};
+
+iniciarSesion()
+
+
+
+// for (let index = 0; index < 250; index++) {
+//   iniciarSesion();
+// }
+// iniciarSesion()
+/* Se recomienda que una función se llame bajo el camelCase */
+
