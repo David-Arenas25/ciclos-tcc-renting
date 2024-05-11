41cb489af9b9fee21df4cedc6ffa7faf5e8b70ae
jaimezzapata <zapataval2304@gmail.com>
2024-05-11 10:14:24 -0500

    Se crean funcion de consulta, retiro y transferencia

--- funciones.js
+++ funciones.js
@@ -82,13 +82,47 @@
 */
 function cajeroElectronico(usuario) {
     console.log('Bienvenido... ' + usuario)
+    let saldo = 1000000 /*Returno y variable global*/
+    let valor = 0
+    let opcion = Number(prompt('Seleccione: \n1 - Consultar Saldo\n2 - Retirar Dinero\n3 - Transferir Dinero'))
+    switch (opcion) {
+        case 1:
+            console.log(saldo)
+            break;
+        case 2:
+            valor = Number(prompt('Ingrese el valor a retirar: '))
+            if (valor > saldo) {
+                console.log(`El valor a retirar: ${valor} es superior a su saldo actual: ${saldo}`)
+            } else {
+                saldo -= valor
+                console.log(`Hizo un retiro por valor de: ${valor}. Su nuevo saldo es: ${saldo}`)
+            }
+            break;
+        case 3:
+            valor = Number(prompt('Ingrese el valor a transferir: '))
+            if (valor > saldo) {
+                console.log(`El valor a transferir: ${valor} es superior a su saldo actual: ${saldo}`)
+            } else {
+                saldo -= valor
+                console.log(`Hizo una transferencia por valor de: ${valor}. Su nuevo saldo es: ${saldo}`)
+            }
+            break;
+    }
 }
 let intentos = 2
 while (intentos >= 0) {
     let usuario = prompt('Ingrese su usuario: ')
     let contrasena = Number(prompt('Ingrese su contrasena: '))
     if (usuario === 'jaime' && contrasena === 1234) {
-        cajeroElectronico(usuario)
+        let repetir = true
+        while (repetir) {
+            cajeroElectronico(usuario)
+            let continuar = Number(prompt('¿Desea cerrar su sesión: \n1 - No\n2 - Si'))
+            if (continuar === 2) {
+                repetir = false
+            }
+        }
+        intentos = -1
     } else {
         // alert('Error de credenciales, tiene ' + intentos + ' restantes ' + intentos)
         if (intentos === 0) {
